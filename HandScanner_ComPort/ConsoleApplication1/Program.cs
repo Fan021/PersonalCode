af94b1128630335d7de7d9d98330be63cf40d6fd
Fan <Xueneng.Fan@KOSTAL.COM>
2021-03-15 09:58:23 +0800

    2021.3.15

---
+++ HandScanner_ComPort/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandScanner_ComPort;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _scanner = new HandScanner(5, 115200);
+            while (true)
+            {
+                if (_scanner.IsBarcodeReady())
+                {
+                    Console.WriteLine(_scanner.GetBarcode());
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    }
+}
---
+++ HandScanner_ComPort/HandScanner_ComPort/HandScanner.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace HandScanner_ComPort
+{
+    public class HandScanner
+    {
+        private SerialPort _device;
+        public HandScanner(int port, int baudrate)
+        {
+            try
+            {
+                _device = new SerialPort("com" + port, baudrate);
+                if (!_device.IsOpen)
+                {
+                    _device.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Init Failed: " + ex.Message);
+            }
+        }
+
+        public bool IsBarcodeReady()
+        {
+            try
+            {
+                if (_device.BytesToRead > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to read byte from com: " + ex.Message);
+            }
+        }
+
+        public string GetBarcode()
+        {
+            try
+            {
+                var numOfBytes = _device.BytesToRead;
+                var receive = new char[numOfBytes];
+                var barcode = "";
+                _device.Read(receive,0,numOfBytes);
+                for (int i = 0; i < receive.Count(); i++)
+                {
+                    barcode += receive[i];
+                }
+                _device.DiscardInBuffer();
+                return barcode;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to get barcode: "+ex.Message);
+            }
+        }
+    }
+}
